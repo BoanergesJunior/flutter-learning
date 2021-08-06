33bb239feaf97fe16e60be2103bf0dd4e15e3090
BoanergesJunior <boanerges_junior@hotmail.com>
2021-08-06 18:05:03 -0300

    transaction_user created (refactor)

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import './components/transaction_list.dart';
-import './components/transaction_form.dart';
+import './components/transaction_user.dart';
 
 void main() => runApp(ExpensesApp());
 
@@ -15,16 +13,6 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(
-        id: 't1',
-        title: 'Novo tenis de corrida',
-        value: 310.76,
-        date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Conta de luz', value: 211.30, date: DateTime.now()),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,8 +29,7 @@ class MyHomePage extends StatelessWidget {
               child: Text('Grafico'),
             ),
           ),
-          TransactionList(_transactions),
-          TransactionForm()
+          TransactionUser(),
         ],
       ),
     );
