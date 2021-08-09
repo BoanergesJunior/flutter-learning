b72b5ed3a226f9baf31b692e957b560385003548
BoanergesJunior <boanerges_junior@hotmail.com>
2021-08-09 17:13:11 -0300

    add images

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -10,50 +10,67 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final item = transactions[index];
-
-          return Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Theme.of(context).primaryColor, width: 2)),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${item.value.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
+      height: 300,
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                SizedBox(height: 30),
+                Text(
+                  'Nenhuma transação cadastrada',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      item.title,
-                      style: Theme.of(context).textTheme.headline6
-                    ),
-                    Text(
-                      DateFormat('d MMM y').format(item.date),
-                      style: TextStyle(color: Colors.grey),
-                    )
-                  ],
+                SizedBox(height: 40),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
                 )
               ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final item = transactions[index];
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).primaryColor,
+                                width: 2)),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${item.value.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(item.title,
+                              style: Theme.of(context).textTheme.headline6),
+                          Text(
+                            DateFormat('d MMM y').format(item.date),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
